7dbf1ab6e88cfbdfe5c008e14fc347be7292c4fd
dhivya-eswaran-25 <e.dhivya74@gmail.com>
2026-05-24 00:10:21 +0530

    update config

--- src/test/java/config/Configuration.java
+++ src/test/java/config/Configuration.java
@@ -1,6 +1,7 @@
 package config;
-import org.aeonbits.owner.Config.*;
+import org.aeonbits.owner.Config;
 import org.aeonbits.owner.Config.Sources;
+import org.aeonbits.owner.Config.Key;
 
 @Sources({"classpath:config.properties"})
 public interface Configuration extends Config {
